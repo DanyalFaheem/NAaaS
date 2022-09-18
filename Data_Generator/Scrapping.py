a90f79591cf371d16780c84ef1c1d26d139f193c
Danyal Faheem <danyalfaheem@gmail.com>
2022-09-18 22:34:43 +0500

    Added Spatial Specificity Working

---
+++ Data_Generator/Scrapping.py
@@ -0,0 +1,40 @@
+from time import sleep
+from urllib.request import Request, urlopen
+from datetime import date
+import os
+
+
+class Scrapper:
+    def __init__(self):
+        todays_date = date.today()
+        path = str(todays_date.year)
+        try:
+            os.mkdir(path)
+        except:
+            pass
+
+    def req(self, link):
+        req = Request(
+            url=link,
+            headers={'User-Agent': 'Mozilla/5.0'})
+        while (True):
+            try:
+                webpage = urlopen(req).read()
+                break
+
+            except ValueError as e:
+                print('\n')
+                print(e)
+
+            except Exception as e:
+                print('\n')
+                print(e)
+                print(
+                    "\n\nWebsite blocked the request, trying again in 5 seconds")
+                sleep(5)  # sleep to avoid blocking
+                os.system("clear")
+                webpage = urlopen(req).read()
+        return webpage
+
+    def savefile(self, name, data):
+        data.to_csv(name)
