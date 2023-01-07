529b7ba560d97dae189ebe47d8bcc0831a5e8213
Mehmood Amjad <mehmood9501@gmail.com>
2023-01-07 17:15:44 +0500

    Spark Connection made with pyspark2

---
+++ Spark/my_job.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+df = spark.read.csv("islamabad.csv", header=True)
+df.select("column_name").count()
+df.show()
+
