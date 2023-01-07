dd9040b44474a093b0b67473109f5744cecddc84
Mehmood Amjad <mehmood9501@gmail.com>
2023-01-07 19:00:09 +0500

    Spark done

--- Spark/my_job.py
+++ Spark/my_job.py
@@ -1,12 +1,30 @@
+# Import the necessary modules
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
-spark = SparkSession \
-    .builder \
-    .appName("Python Spark SQL basic example") \
-    .config("spark.some.config.option", "some-value") \
-    .getOrCreate()
+# Create a SparkSession
+spark = SparkSession.builder \
+   .appName("My App") \
+   .config("spark.some.config.option", "some-value") \
+   .getOrCreate()
 
-df = spark.read.csv("islamabad.csv", header=True)
-df.select("column_name").count()
+# Create a simple Python list
+data = [("Alice", 1), ("Bob", 2), ("Charlie", 3), ("David", 4), ("Edward", 5)]
+
+# Convert the list to a Spark DataFrame
+df = spark.createDataFrame(data, ["name", "age"])
+
+# Print the DataFrame
 df.show()
 
+# Select the "name" column and show the first 10 rows
+df.select("name").show(10)
+
+# Filter the DataFrame to only include people who are 25 or older
+df.filter(df["age"] >= 25).show()
+
+# Calculate the average age
+df.agg(avg(df["age"])).show()
+
+# Stop the SparkSession
+spark.stop()
